e581ad2636040f5729f9c6253b0e277fdd4e7064
LeoDm123 <leo.meiners1@gmail.com>
2025-02-08 16:59:33 -0300

    cambios variales

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
-  API_URL_GET_BECAS: "https://todobeca-backend.vercel.app/beca/getBecas",
+  API_URL_GET_BECAS: "https://backend-8vsm.onrender.com/beca/getBecas",
   API_URL_GET_PARAMETROS:
-    "https://todobeca-backend.vercel.app/parametros/getParametros",
-  API_URL_POST_USUARIO: "https://todobeca-backend.vercel.app/auth/createUser",
-  API_URL_POST_LOGIN: "https://todobeca-backend.vercel.app/auth/userLogin",
-  API_URL_GET_USER: "https://todobeca-backend.vercel.app/auth/getUserByEmail",
-  API_URL_UPDATE_USER: "https://todobeca-backend.vercel.app/auth/updateUser",
+    "https://backend-8vsm.onrender.com/parametros/getParametros",
+  API_URL_POST_USUARIO: "https://backend-8vsm.onrender.com/auth/createUser",
+  API_URL_POST_LOGIN: "https://backend-8vsm.onrender.com/auth/userLogin",
+  API_URL_GET_USER: "https://backend-8vsm.onrender.com/auth/getUserByEmail",
+  API_URL_UPDATE_USER: "https://backend-8vsm.onrender.com/auth/updateUser",
 };
