a9954e6dc5cc0a9ffaaa9580d27ac04118e1810f
LeoDm123 <leo.meiners1@gmail.com>
2025-02-08 16:39:31 -0300

    cambios variales

---
+++ config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  API_URL_GET_BECAS: "https://todobeca-backend.vercel.app/beca/getBecas",
+  API_URL_GET_PARAMETROS:
+    "https://todobeca-backend.vercel.app/parametros/getParametros",
+  API_URL_POST_USUARIO: "https://todobeca-backend.vercel.app/auth/createUser",
+  API_URL_POST_LOGIN: "https://todobeca-backend.vercel.app/auth/userLogin",
+  API_URL_GET_USER: "https://todobeca-backend.vercel.app/auth/getUserByEmail",
+  API_URL_UPDATE_USER: "https://todobeca-backend.vercel.app/auth/updateUser",
+};
