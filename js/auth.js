47ae674b4bfc058ebd1025658d1d864ea85fd781
LeoDm123 <leo.meiners1@gmail.com>
2025-04-14 14:37:12 -0300

    cambios de formato

--- js/auth.js
+++ js/auth.js
@@ -45,7 +45,7 @@ function actualizarUI() {
   } else {
     // Si no hay usuario, mostrar el botón de "Iniciar Sesión"
     document.getElementById("authContainer").innerHTML = `
-          <button id="btn-auth" class="btn btn-auth w-50 btn-pill" onclick="window.location.href='/login.html'">
+          <button id="btn-auth" class="btn btn-auth btn-pill" onclick="window.location.href='/login.html'">
             Iniciar Sesión
           </button>
         `;
